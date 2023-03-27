da387af1f81b7cac1d3f6c4865c0623ed4ad9bc2
vd5813 <vd5813@gmail.com>
2023-03-27 19:07:04 +0530

    REST APIs for Goals

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require('../backend/middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
-const { errorHandler } = require('../backend/middleware/errorMiddleware')
+
+connectDB();
 
 const app = express();
 
